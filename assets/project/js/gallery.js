462be19152b73537fcac293d22907948f9551e0b
Serg <comradetrevel@gmail.com>
2021-02-21 08:56:18 +0300

    Добавлены scss js файлы

---
+++ assets/project/js/gallery.js
@@ -0,0 +1,17 @@
+function bGallery() {
+    const bigimg = document.querySelector('.b-gallery__big-img');
+    const smallimgs = document.querySelectorAll('.b-gallery__preview img');
+
+
+    smallimgs.forEach(smallimg => {
+        smallimg.addEventListener('click', function () {
+            const smallerImg = this.src;
+            const biggerImg = smallerImg.replace('small', 'big');
+            bigimg.src = biggerImg;
+            smallimgs.forEach(smallimg => smallimg.classList.remove('b-active'));
+            smallimg.classList.add('b-active');
+        });
+    });
+}
+
+bGallery();
